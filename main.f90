a846af020e715c9eff2e597357d0757f79627b70
Masanori Hanada <37608759+masanorihanada@users.noreply.github.com>
2025-04-06 18:46:57 +0100

    Update main.f90

--- main.f90
+++ main.f90
@@ -3,7 +3,7 @@
 !######                                                               #########
 !######                 written by Masanori Hanada                    #########
 !######                                                               #########
-!####  The definition of g^2 is factor 2 different from the orbifold lattice ##
+!######  The definition of g^2 is factor 2N off from the orbifold lattice #####
 !##############################################################################
 !Mersenne twister.
 include 'mt19937.f90'
